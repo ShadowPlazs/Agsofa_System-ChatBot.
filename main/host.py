3cf0b5f5bb6f5b81d797823f1957bea4a3fb3e1e
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2022-01-02 18:40:01 +0800

    Update and rename host.py to main/host.py

--- main/host.py
+++ main/host.py
@@ -2,4 +2,4 @@ def on_connect(rtx):
   connection.reached =
     print("Connection connected! Send a command to start the bot. Ex: as/help")
    
-connection.token(.env("PASSWORD.BOTCHAT"))
+connection.token(.env("PASSWORD_BOTCHAT"))
